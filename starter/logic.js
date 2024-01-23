369b9672da8f1af2cc2cdd315f9fb7d5aa7ad489
fj010101 <151868892+fj010101@users.noreply.github.com>
2024-01-23 19:53:09 +0000

    Update logic.js
    Added the function to colour-code timeblocks based on past, present, or future

--- starter/logic.js
+++ starter/logic.js
@@ -3,3 +3,24 @@
 $(document).ready(function () {
   // Display the current day at the top of the calendar
   $("#currentDay").text(dayjs().format("dddd, MMMM D"));
+
+// Function to colour-code timeblocks based on past, present, or future
+  function updateHourstyles() {
+    var currentHour = dayjs().hour();
+
+  $(".time-block").each(function () {
+    var blockHour = parseInt($(this).attr("data.hour"));
+    var $textarea = $(this).find("textarea");
+
+
+     $textarea.removeClass("past present future");
+
+          if (clockHour < currentHour) {
+           $textarea.addClass("past");
+          } else if (blockhour === currentHour) {
+            $textarea.addClass("present")
+          } else {
+            $textarea.addClass("future");
+          }
+  });
+  }
