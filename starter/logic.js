0bfc548ab359ab0450c3a6666e9a47c0dc4a3551
fj010101 <fozia.jaylani@hotmail.com>
2024-01-23 20:15:29 +0000

    incorrect keyword removed

--- starter/logic.js
+++ starter/logic.js
@@ -24,3 +24,25 @@ $(document).ready(function () {
           }
   });
   }
+
+  // Function to load events from local storage
+  function loadEvents() {
+    $(".time-block").each(function () {
+    var blockHour = $(this).attr("data-hour");
+    var savedEvent = localStorage.getItem("event-" + blockHour);
+    if (savedEvent) {
+      $ (this).find("textarea").val(savedEvent); 
+        }
+  });
+}
+})
+
+// Function to save events to local storage
+
+ $("saveBtn").on("click", function (){
+  var $hourBlock = $(this).parent();
+  var hour = $hourBlock.attr("data-hour"); 
+  var eventText = $hourBlock.find("textarea").val();
+
+  localStorage.setItem("event-" + hour, eventText);
+});
